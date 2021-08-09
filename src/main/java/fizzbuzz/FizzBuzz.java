6a562404857403a5cb86ab11066dfdf51954614e
Mario Schuetz <mayo2@gmx.de>
2021-08-09 11:09:36 +0200

    Remove unused import. Format code

--- src/main/java/fizzbuzz/FizzBuzz.java
+++ src/main/java/fizzbuzz/FizzBuzz.java
@@ -4,20 +4,18 @@ import java.util.Arrays;
 
 public class FizzBuzz {
 
-    private static int[] numbers;
-
-    public static void main(String[] args) {
-        numbers = new int[100];
-        Arrays.setAll(numbers, i -> i+1);
-        for(int i : numbers) {
-            System.out.println(i + " " + fizzBuzzNumber(i)) ;
-        }
+  public static void main(String[] args) {
+    int[] numbers = new int[100];
+    Arrays.setAll(numbers, i -> i + 1);
+    for (int i : numbers) {
+      System.out.println(i + " " + fizzBuzzNumber(i));
     }
+  }
 
-    public static String fizzBuzzNumber(int number) {
-        if(number%3 == 0 && number%5 == 0) return "FizzBuzz";
-        else if(number%3 == 0) return "Fizz";
-        else if(number%5 == 0) return "Buzz";
-        return String.valueOf(number);
-    }
+  public static String fizzBuzzNumber(int number) {
+    if (number % 3 == 0 && number % 5 == 0) return "FizzBuzz";
+    else if (number % 3 == 0) return "Fizz";
+    else if (number % 5 == 0) return "Buzz";
+    return String.valueOf(number);
+  }
 }
--- src/test/java/fizzbuzz/FizzBuzzTest.java
+++ src/test/java/fizzbuzz/FizzBuzzTest.java
@@ -1,6 +1,5 @@
 package fizzbuzz;
 
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
@@ -11,109 +10,108 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 class FizzBuzzTest {
 
+  @Test
+  @DisplayName("Should return input as String")
+  void shouldReturnANumberString() {
+    // Given
+    int number = 1;
+    // When
+    String fizzBuzzAnswer = fizzBuzzNumber(number);
+    // Then
+    assertThat(fizzBuzzAnswer.equals(Integer.toString(number))).isTrue();
+  }
 
-    @Test
-    @DisplayName("Should return input as String")
-    void shouldReturnANumberString() {
-        // Given
-        int number = 1;
-        // When
-        String fizzBuzzAnswer = fizzBuzzNumber(number);
-        // Then
-        assertThat(fizzBuzzAnswer.equals(Integer.toString(number))).isTrue();
-    }
+  @ParameterizedTest
+  @CsvSource({
+      "1, TRUE",
+      "2, TRUE",
+      "3, FALSE",
+      "4, TRUE",
+      "5, FALSE",
+      "15, FALSE"
+  })
+  @DisplayName("Should return number as String if not dividable by 3 and/or 5")
+  void shouldReturnNumberAsString(int number, boolean expected) {
+    // When
+    String fizzBuzzAnswer = fizzBuzzNumber(number);
+    // Then
+    assertThat(fizzBuzzAnswer.equals(Integer.toString(number))).isEqualTo(expected);
+  }
 
-    @ParameterizedTest
-    @CsvSource({
-            "1, TRUE",
-            "2, TRUE",
-            "3, FALSE",
-            "4, TRUE",
-            "5, FALSE",
-            "15, FALSE"
-    })
-    @DisplayName("Should return number as String if not dividable by 3 and/or 5")
-    void shouldReturnNumberAsString(int number, boolean expected) {
-        // When
-        String fizzBuzzAnswer = fizzBuzzNumber(number);
-        // Then
-        assertThat(fizzBuzzAnswer.equals(Integer.toString(number))).isEqualTo(expected);
-    }
+  @ParameterizedTest
+  @CsvSource({
+      "1, FALSE",
+      "2, FALSE",
+      "3, TRUE",
+      "4, FALSE",
+      "5, FALSE",
+      "6, TRUE",
+      "15, FALSE"
+  })
+  @DisplayName("Should return Fizz if only dividable by 3")
+  void shouldReturnFizz(int number, boolean expected) {
+    // When
+    String fizzBuzzAnswer = fizzBuzzNumber(number);
+    // Then
+    assertThat(fizzBuzzAnswer.equals("Fizz")).isEqualTo(expected);
+  }
 
-    @ParameterizedTest
-    @CsvSource({
-            "1, FALSE",
-            "2, FALSE",
-            "3, TRUE",
-            "4, FALSE",
-            "5, FALSE",
-            "6, TRUE",
-            "15, FALSE"
-    })
-    @DisplayName("Should return Fizz if only dividable by 3")
-    void shouldReturnFizz(int number, boolean expected) {
-        // When
-        String fizzBuzzAnswer = fizzBuzzNumber(number);
-        // Then
-        assertThat(fizzBuzzAnswer.equals("Fizz")).isEqualTo(expected);
-    }
+  @ParameterizedTest
+  @CsvSource({
+      "1, FALSE",
+      "2, FALSE",
+      "3, FALSE",
+      "4, FALSE",
+      "5, TRUE",
+      "6, FALSE",
+      "10, TRUE",
+      "15, FALSE"
+  })
+  @DisplayName("Should return Buzz if only dividable by 5")
+  void shouldReturnBuzz(int number, boolean expected) {
+    // When
+    String fizzBuzzAnswer = fizzBuzzNumber(number);
+    // Then
+    assertThat(fizzBuzzAnswer.equals("Buzz")).isEqualTo(expected);
+  }
 
-    @ParameterizedTest
-    @CsvSource({
-            "1, FALSE",
-            "2, FALSE",
-            "3, FALSE",
-            "4, FALSE",
-            "5, TRUE",
-            "6, FALSE",
-            "10, TRUE",
-            "15, FALSE"
-    })
-    @DisplayName("Should return Buzz if only dividable by 5")
-    void shouldReturnBuzz(int number, boolean expected) {
-        // When
-        String fizzBuzzAnswer = fizzBuzzNumber(number);
-        // Then
-        assertThat(fizzBuzzAnswer.equals("Buzz")).isEqualTo(expected);
-    }
+  @ParameterizedTest
+  @CsvSource({
+      "1, FALSE",
+      "2, FALSE",
+      "3, FALSE",
+      "4, FALSE",
+      "5, FALSE",
+      "6, FALSE",
+      "15, TRUE",
+      "20, FALSE",
+      "30, TRUE"
+  })
+  @DisplayName("Should return FizzBuzz if dividable by 3 and 5")
+  void shouldReturnFizzBuzz(int number, boolean expected) {
+    // When
+    String fizzBuzzAnswer = fizzBuzzNumber(number);
+    // Then
+    assertThat(fizzBuzzAnswer.equals("FizzBuzz")).isEqualTo(expected);
+  }
 
-    @ParameterizedTest
-    @CsvSource({
-            "1, FALSE",
-            "2, FALSE",
-            "3, FALSE",
-            "4, FALSE",
-            "5, FALSE",
-            "6, FALSE",
-            "15, TRUE",
-            "20, FALSE",
-            "30, TRUE"
-    })
-    @DisplayName("Should return FizzBuzz if dividable by 3 and 5")
-    void shouldReturnFizzBuzz(int number, boolean expected) {
-        // When
-        String fizzBuzzAnswer = fizzBuzzNumber(number);
-        // Then
-        assertThat(fizzBuzzAnswer.equals("FizzBuzz")).isEqualTo(expected);
-    }
-
-    @ParameterizedTest
-    @CsvSource({
-            "1, '1'",
-            "2, '2'",
-            "3, 'Fizz'",
-            "4, '4'",
-            "5, 'Buzz'",
-            "6, 'Fizz'",
-            "15, 'FizzBuzz'",
-            "20, 'Buzz'",
-            "30, 'FizzBuzz'"
-    })
-    @DisplayName("Should return FizzBuzz if dividable by 3 and 5")
-    void shouldReturnFizzBuzz(int number, String expected) {
-        // When
-        String fizzBuzzAnswer = fizzBuzzNumber(number);
-        // Then
-        assertThat(fizzBuzzAnswer).isEqualTo(expected);
-    }
+  @ParameterizedTest
+  @CsvSource({
+      "1, '1'",
+      "2, '2'",
+      "3, 'Fizz'",
+      "4, '4'",
+      "5, 'Buzz'",
+      "6, 'Fizz'",
+      "15, 'FizzBuzz'",
+      "20, 'Buzz'",
+      "30, 'FizzBuzz'"
+  })
+  @DisplayName("Should return FizzBuzz if dividable by 3 and 5")
+  void shouldReturnFizzBuzz(int number, String expected) {
+    // When
+    String fizzBuzzAnswer = fizzBuzzNumber(number);
+    // Then
+    assertThat(fizzBuzzAnswer).isEqualTo(expected);
+  }
 }
