bf392e68dc489fe046a86cd793d401d0a3739aa7
Mario Schuetz <mayo2@gmx.de>
2021-08-11 14:10:37 +0200

    Remove unused import

--- src/test/java/anagram/AnagramGeneratorTest.java
+++ src/test/java/anagram/AnagramGeneratorTest.java
@@ -3,7 +3,6 @@ package anagram;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
-import java.util.Set;
 
 import static anagram.AnagramGenerator.generateAnagram;
 import static org.assertj.core.api.Assertions.assertThat;
