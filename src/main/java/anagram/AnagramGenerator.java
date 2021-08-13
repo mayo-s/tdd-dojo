e3751d7f1ed432b50512cc6754c46243f2c832d0
Mario Schuetz <mayo2@gmx.de>
2021-08-13 14:13:39 +0200

    Rename method

--- src/main/java/anagram/AnagramGenerator.java
+++ src/main/java/anagram/AnagramGenerator.java
@@ -5,22 +5,21 @@ import java.util.List;
 
 public class AnagramGenerator {
 
-  private static ArrayList<String> anagrams;
+  private static ArrayList<String> anagrams = new ArrayList<>();
 
   public static void main(String[] args) {
-    anagrams = new ArrayList<>();
-    for(String word : generateAnagram("word".toCharArray(), 0)) System.out.println(word);
+    for (String word : generateAnagrams("word".toCharArray(), 0)) System.out.println(word);
   }
 
-  public static List<String> generateAnagram(char[] anagram, int i) {
+  public static List<String> generateAnagrams(char[] anagram, int i) {
 
-    if (i == anagram.length - 1) anagrams.add(String.valueOf(anagram));
+    if (i >= anagram.length - 1) anagrams.add(String.valueOf(anagram));
     else {
       for (int j = i; j < anagram.length; j++) {
         char c = anagram[i];
         anagram[i] = anagram[j];
         anagram[j] = c;
-        generateAnagram(anagram, i + 1);
+        generateAnagrams(anagram, i + 1);
         c = anagram[i];
         anagram[i] = anagram[j];
         anagram[j] = c;
--- src/test/java/anagram/AnagramGeneratorTest.java
+++ src/test/java/anagram/AnagramGeneratorTest.java
@@ -4,28 +4,28 @@ import org.junit.jupiter.api.Test;
 
 import java.util.List;
 
-import static anagram.AnagramGenerator.generateAnagram;
+import static anagram.AnagramGenerator.generateAnagrams;
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class AnagramGeneratorTest {
   private char[] word = "word".toCharArray();
 
   @Test
-  void shouldReturnSetOfStrings() {
-    assertThat(generateAnagram(word, 0) instanceof List).isTrue();
+  void shouldReturnListOfStrings() {
+    assertThat(generateAnagrams(word, 0) instanceof List).isTrue();
   }
 
   @Test
   void shouldContainOriginWord() {
-    assertThat(generateAnagram(word, 0)).contains("word");
+    assertThat(generateAnagrams(word, 0)).contains("word");
   }
 
   @Test
   void shouldContainAnagramOfOriginWord() {
-    assertThat(generateAnagram(word, 0)).contains("wodr");
-    assertThat(generateAnagram(word, 0)).contains("wrod");
-    assertThat(generateAnagram(word, 0)).contains("wrdo");
-    assertThat(generateAnagram(word, 0)).contains("wdro");
-    assertThat(generateAnagram(word, 0)).contains("wdor");
+    assertThat(generateAnagrams(word, 0)).contains("wodr");
+    assertThat(generateAnagrams(word, 0)).contains("wrod");
+    assertThat(generateAnagrams(word, 0)).contains("wrdo");
+    assertThat(generateAnagrams(word, 0)).contains("wdro");
+    assertThat(generateAnagrams(word, 0)).contains("wdor");
   }
 }
